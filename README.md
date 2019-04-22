13e9468483b3b87428e3388b9f7720d33f80a402
adammmmm <adammmmm@users.noreply.github.com>
2019-04-22 10:28:34 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,47 @@
+### Hitless Key Rollover
+
+This is one of my first forays into programming, so there are most likely loads of "wrong" ways of doing what I'm doing.
+
+The idea got to me when comparing macsec-options on juniper/cisco routers where juniper don't have time-based SAK rekeys, the recommendation is instead to keep the CKN/CAK rekeys short, hence this script. 
+
+Don't want to manually send out new keys every other day.
+
+---
+
+##### Instructions
+
+data.yml contains a couple variables you should change:
+
+- USER
+
+- PASS
+
+- ROLLINTERVAL
+
+- KEYCHAIN-NAME
+
+- HOSTS
+
+Guessing they should be pretty self explanatory, USER/PASS will be the credentials for the ssh connection to the routers. ROLLINTERVAL is how often the keys will be rolled over in hours. KEYCHAIN-NAME is:
+
+[ edit security authentication-key-chains key-chain **THIS** ]
+
+It currently needs to already be configured with one key for the script to work.
+
+And of course HOSTS should be all your routers ip-addresses.
+
+---
+
+##### What it does
+
+It will create a dictionary of 64 key ids. All with maximum CKN+CAK lengths of random hex strings. The start-time will be current time plus the ROLLINTERVAL. It'll then log in to all HOSTS and show the current key-chains and see if KEYCHAIN-NAME exists.
+
+After that some checks occur that the active send key and receive key are the same, that there are no next keys/times in the chain and appends the active key to a list.
+
+Then comes a check that we've got as many keys appended to the list as we have HOSTS after which we'll make sure every key is the same by making it a set instead of list.
+
+This information allows us to create the template jinja2 file populated with the 64 key id dict which will ignore the active key.
+
+Eventually, we connect to all devices again and commit the configuration with a comment that will let people know what's happened.
+
+
---
+++ keychain.py
@@ -0,0 +1,110 @@
+import json
+import logging
+import random
+import string
+import sys
+from datetime import datetime, timedelta
+
+import yaml
+from jinja2 import Template
+from jnpr.junos import Device
+from jnpr.junos.utils.config import Config
+from jnpr.junos.utils.start_shell import StartShell
+
+lgr = logging.getLogger('keychain')
+lgr.setLevel(logging.INFO)
+
+logfh = logging.FileHandler('keychain.log')
+logfh.setLevel(logging.INFO)
+
+frmt = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logfh.setFormatter(frmt)
+lgr.addHandler(logfh)
+
+
+def generate_hex(self):
+    ''' Function to create hex string of passed length'''
+    rand = ''.join(random.SystemRandom().choice(string.hexdigits) for _ in range(self))
+    return str.lower(rand)
+
+with open("data.yml") as fh:
+    data = yaml.load(fh.read())
+
+DATA = {}
+for i in range(64):
+    DATA["CKN" + str(i)] = generate_hex(64)
+    DATA["CAK" + str(i)] = generate_hex(32)
+    DATA["ROLL" + str(i)] = (datetime.now() + timedelta(hours=data['ROLLINTERVAL']) * (i+1)).strftime('%Y-%m-%d.%H:%M:%S')
+
+usedid = []
+
+for router in data['HOSTS']:
+    print(f"Checking {router}")
+    try:
+        with Device(host=router, user=data['USER'], passwd=data['PASS'], port=22) as dev:
+            with StartShell(dev) as ss:
+                tupledata = ss.run('cli -c "show security keychain | display json | no-more"')
+                listdata = list(tupledata)
+                liststr = listdata[1]
+                # As shell mode shows both the command as first line and a prompt as last, both non-json, we need to remove them
+                nofirstorlastline = '\n'.join(liststr.split('\n')[1:-1])
+                jsoned = json.loads(nofirstorlastline)
+                jsonedkeychain = jsoned['hakr-keychain-information'][0]['hakr-keychain']
+                for keyid in jsonedkeychain:
+                    if keyid['hakr-keychain-name'][0]['data'] == data['KEYCHAIN-NAME']:
+                        hkask = keyid['hakr-keychain-active-send-key'][0]['data']
+                        hkark = keyid['hakr-keychain-active-receive-key'][0]['data']
+                        hknsk = keyid['hakr-keychain-next-send-key'][0]['data']
+                        hknrk = keyid['hakr-keychain-next-receive-key'][0]['data']
+                        hknkt = keyid['hakr-keychain-next-key-time'][0]['data']
+                        if hkask == hkark:
+                            if hknsk and hknrk and hknkt == "None":
+                                usedid.append(hkask)
+                            else:
+                                print(f"Next send key {hknsk}, next receive key {hknrk}, rolling over in {hknkt}")
+                                sys.exit(0)
+                        else:
+                            print(f"Send key: {hkask}, Receive key: {hkark}")
+                            sys.exit(1)
+                    else:
+                        print(f"Keychain {data['KEYCHAIN-NAME']} not found")
+                        sys.exit(1)
+    except Exception as err:
+        print(f"PyEZ checking exception, {err}")
+        sys.exit(1)
+
+if len(usedid) == len(data['HOSTS']):
+    if len(set(usedid)) == 1:
+        print(f"All routers replied with the same key id: {usedid[0]}")
+    else:
+        print("Router key id sync issue")
+        sys.exit(1)
+else:
+    print(f"Only got a reply from {len(usedid)} out of {len(data['HOSTS'])} devices")
+    sys.exit(1)
+
+with open('temp.j2', mode="w") as twr:
+    for i in range(64):
+        if i == int(usedid[0]):
+            continue
+        twr.write(f'set security authentication-key-chains key-chain {data["KEYCHAIN-NAME"]} key {i} secret {{{{CAK{i}}}}}\n')
+        twr.write(f'set security authentication-key-chains key-chain {data["KEYCHAIN-NAME"]} key {i} key-name {{{{CKN{i}}}}}\n')
+        twr.write(f'set security authentication-key-chains key-chain {data["KEYCHAIN-NAME"]} key {i} start-time "{{{{ROLL{i}}}}}"\n')
+
+with open('temp.j2') as t_fh:
+    t_format = t_fh.read()
+
+template = Template(t_format)
+lgr.info(template.render(DATA))
+
+for router in data['HOSTS']:
+    print(f"Configuring {router}")
+    try:
+        with Device(host=router, user=data['USER'], passwd=data['PASS'], port=22) as dev:
+            conf = Config(dev)
+            conf.load(template.render(DATA), format='set')
+            conf.pdiff()
+            conf.commit(comment="Updated keychain")
+    except Exception as err:
+        print(f"PyEZ configuration exception {err}")
+        sys.exit(1)

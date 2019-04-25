bba977a6a9bb615c7d15edbc4b19870c7771cafe
adammmmm <adammmmm@users.noreply.github.com>
2019-04-25 11:15:24 +0200

    quotation marks

--- keychain.py
+++ keychain.py
@@ -31,15 +31,15 @@ def generate_hex(self):
 
 def generate_time(self):
     ''' Helper function to calculate timedeltas '''
-    nexttime = datetime.now() + timedelta(hours=data['ROLLINTERVAL'])
-    addtime = nexttime + (timedelta(hours=data['ROLLINTERVAL']) * self)
+    nexttime = datetime.now() + timedelta(hours=data["ROLLINTERVAL"])
+    addtime = nexttime + (timedelta(hours=data["ROLLINTERVAL"]) * self)
     return addtime
 
-with open("data.yml") as fh:
+with open('data.yml') as fh:
     data = yaml.load(fh.read(), Loader=yaml.SafeLoader)
 
-if data['ROLLINTERVAL'] <= 1:
-    print("Increase the ROLLINTERVAL in data.yml")
+if data["ROLLINTERVAL"] <= 1:
+    print('Increase the ROLLINTERVAL in data.yml')
     sys.exit(0)
 
 DATA = {}
@@ -50,47 +50,47 @@ for i in range(52):
 
 usedid = []
 
-for router in data['HOSTS']:
-    print(f"Checking {router}")
+for router in data["HOSTS"]:
+    print(f'Checking {router}')
     try:
-        with Device(host=router, user=data['USER'], passwd=data['PASS'], port=22) as dev:
+        with Device(host=router, user=data["USER"], passwd=data["PASS"], port=22) as dev:
             dictdata = dev.rpc.get_hakr_keychain_information({"format" : "json"})
             if dictdata:
-                jsonedkeychain = dictdata['hakr-keychain-information'][0]['hakr-keychain']
+                jsonedkeychain = dictdata["hakr-keychain-information"][0]["hakr-keychain"]
                 for keyid in jsonedkeychain:
-                    if keyid['hakr-keychain-name'][0]['data'] == data['KEYCHAIN-NAME']:
-                        hkask = keyid['hakr-keychain-active-send-key'][0]['data']
-                        hkark = keyid['hakr-keychain-active-receive-key'][0]['data']
-                        hknsk = keyid['hakr-keychain-next-send-key'][0]['data']
-                        hknrk = keyid['hakr-keychain-next-receive-key'][0]['data']
-                        hknkt = keyid['hakr-keychain-next-key-time'][0]['data']
+                    if keyid["hakr-keychain-name"][0]["data"] == data["KEYCHAIN-NAME"]:
+                        hkask = keyid["hakr-keychain-active-send-key"][0]["data"]
+                        hkark = keyid["hakr-keychain-active-receive-key"][0]["data"]
+                        hknsk = keyid["hakr-keychain-next-send-key"][0]["data"]
+                        hknrk = keyid["hakr-keychain-next-receive-key"][0]["data"]
+                        hknkt = keyid["hakr-keychain-next-key-time"][0]["data"]
                         if hkask == hkark:
-                            if hknsk and hknrk and hknkt == "None":
+                            if hknsk and hknrk and hknkt == 'None':
                                 usedid.append(hkask)
                             else:
-                                print(f"Next send key {hknsk}, next receive key {hknrk}, rolling over in {hknkt}")
+                                print(f'Next send key {hknsk}, next receive key {hknrk}, rolling over in {hknkt}')
                                 sys.exit(0)
                         else:
-                            print(f"Send key: {hkask}, Receive key: {hkark}")
+                            print(f'Send key: {hkask}, Receive key: {hkark}')
                             sys.exit(1)
                     else:
-                        print(f"Keychain {data['KEYCHAIN-NAME']} not found")
+                        print(f'Keychain {data["KEYCHAIN-NAME"]} not found')
                         sys.exit(1)
     except Exception as err:
-        print(f"PyEZ checking exception, {err}")
+        print(f'PyEZ checking exception, {err}')
         sys.exit(1)
 
-if len(usedid) == len(data['HOSTS']):
+if len(usedid) == len(data["HOSTS"]):
     if len(set(usedid)) == 1:
-        print(f"All routers replied with the same key id: {usedid[0]}")
+        print(f'All routers replied with the same key id: {usedid[0]}')
     else:
-        print(f"Router key id sync issue, got: {set(usedid)}")
+        print(f'Router key id sync issue, got: {set(usedid)}')
         sys.exit(1)
 else:
-    print(f"Only got a reply from {len(usedid)} out of {len(data['HOSTS'])} devices")
+    print(f'Only got a reply from {len(usedid)} out of {len(data["HOSTS"])} devices')
     sys.exit(1)
 
-with open('temp.j2', mode="w") as twr:
+with open('temp.j2', mode='w') as twr:
     for i in range(52):
         if i == int(usedid[0]):
             continue
@@ -102,20 +102,20 @@ with open('temp.j2') as t_fh:
     t_format = t_fh.read()
 
 template = Template(t_format)
-if data['LOGGING']:
+if data["LOGGING"]:
     lgr.info(template.render(DATA))
 
-for router in data['HOSTS']:
-    print(f"Configuring {router}")
+for router in data["HOSTS"]:
+    print(f'Configuring {router}')
     try:
-        with Device(host=router, user=data['USER'], passwd=data['PASS'], port=22) as dev:
+        with Device(host=router, user=data["USER"], passwd=data["PASS"], port=22) as dev:
             conf = Config(dev)
             conf.load(template.render(DATA), format='set')
-            conf.commit(comment=f"Updated {data['KEYCHAIN-NAME']} keychain")
+            conf.commit(comment=f'Updated {data["KEYCHAIN-NAME"]} keychain')
     except Exception as err:
-        print(f"PyEZ configuration exception, {err}")
+        print(f'PyEZ configuration exception, {err}')
         sys.exit(1)
 try:
-    os.remove("temp.j2")
+    os.remove('temp.j2')
 except OSError as err:
-    print(f"Failed to delete temporary template, {err}")
+    print(f'Failed to delete temporary template, {err}')
